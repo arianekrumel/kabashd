1f0a7b1dd2a27e00a7e8e0a544043eb612fbadf9
sydneyhodge <sydneyahodge@gmail.com>
2016-02-16 16:17:37 -0500

    Game skeleton now updates the state of a game.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,26 @@ module ApplicationHelper
 		@@states = Hash.new
 		@@states_to_responses = Hash.new
 
+		def get_updated_state(action)
+			# Returns the updated state to the user. Also updates the user state.
+			state = @@actions[action]
+			if state != nil
+				# Check if this is a valid state based on where the user is currently at.
+				if @@states_to_actions[@user_state].include? action
+					@user_state = state
+					return "Action: " + action + " has been performed."
+				else
+					# User can not perform this action based on their current state.
+					return "You can not " + action.downcase + " from the " + @user_state.downcase +
+					". Please choose again."
+				end
+			else
+				# This is state is not in this game.
+				return "You're in the " + @user_state.downcase + ". Unfortunately, " + action.downcase +
+				" is not a valid option. Chose again."
+			end 
+		end 
+
 		def get_response_by_state()
 			return @@states_to_responses[@user_state]
 		end 
--- app/helpers/demo_helper.rb
+++ app/helpers/demo_helper.rb
@@ -66,26 +66,6 @@ module DemoHelper
 	    @@states_to_actions['Living Room'] = ['Turn on TV', 'Play piano']
 	    @@states_to_actions['Kitchen'] = ['Go to bedroom', 'Open the fridge', 'Turn on the oven', 'Wash dishes']
 	    @@states_to_actions['Bedroom'] = ['Lay in bed', 'Sit on futon', 'Clean room']
-	  end 
-
-	  def get_updated_state(action)
-	  	# Returns the updated state to the user. Also updates the user state.
-	  	state = @@actions[action]
-	  	if state != nil
-	  		# Check if this is a valid state based on where the user is currently at.
-	  		if @@states_to_actions[@user_state].include? action
-	  			@user_state = state
-	  			return "Action: " + action + " has been performed."
-	  		else
-	  			# User can not perform this action based on their current state.
-	  			return "You can not " + action.downcase + " from the " + @user_state.downcase +
-	  			". Please choose again."
-	  		end
-	  	else
-	  		# This is state is not in this game.
-	  		return "You're in the " + @user_state.downcase + ". Unfortunately, " + action.downcase +
-	  		" is not a valid option. Chose again."
-	  	end 
-	  end 
+	  end
 	end
 end
