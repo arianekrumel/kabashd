e929526c24e38a245ec6b64c65d9a47a83981129
sydneyhodge <sydneyahodge@gmail.com>
2016-03-07 15:39:28 -0500

    Added who was speaking to the output text, put each output dialog in a box, restructured output text to read from bottom to top, and username is now being displayed.

--- app/controllers/sprained_ankle_controller.rb
+++ app/controllers/sprained_ankle_controller.rb
@@ -2,7 +2,7 @@ class SprainedAnkleController < ApplicationController
   include SprainedAnkleHelper
 
   @@sprained_ankle_game = SprainedAnkleGame.new('Start')
-  @username = ""
+  $username
   @watson_question_output = ""
   user_state = ""
 
@@ -16,6 +16,8 @@ class SprainedAnkleController < ApplicationController
 		@game_output = @@sprained_ankle_game.get_game_output()
 		@watson_output =  @@sprained_ankle_game.get_watson_output()
   		@watson_question_output = query_watson(user_input)[1]
+  		@watson_output << "<p> Watson's Brain <p> <div class='card'>#{@watson_question_output}></div>"
+
 		return
 	end
 
@@ -31,19 +33,24 @@ class SprainedAnkleController < ApplicationController
 		@watson_output << "WATSON RETURNS NIL :(. - No Ka-bash'd documents..."
 	elsif user_state == "Name"
 		# Transition to the NAME state without checking validity of action.
+		@@sprained_ankle_game.set_game_output("You <div class='card'>" + user_input + "</div>")
 		@@sprained_ankle_game.set_percent_per_action(user_state)
 		@user_percent = @@sprained_ankle_game.get_percent_per_action()
 
-		@username = user_input
+		$username = user_input
 		user_state = @@sprained_ankle_game.updated_state(user_state)
 
 		@updates = user_state
 		@@sprained_ankle_game.get_all_responses(user_state)
+
+		# @game_output = @@sprained_ankle_game.get_game_output()
+		# @game_output << watson_response
+		# @watson_output =  @@sprained_ankle_game.get_watson_output()
 	elsif watson_response.empty?
   		# This is the first time the index page is being loaded (i.e., there is no user input).
 		@@sprained_ankle_game.get_all_responses()
 	elsif user_state == "Remedy"
-		@@sprained_ankle_game.set_game_output(user_input)
+		@@sprained_ankle_game.set_game_output("You <div class='card'>" + user_input + "</div>")
 		@@sprained_ankle_game.set_remedy_value(watson_response)
 		if @@sprained_ankle_game.move_to_done_state?
 			user_state = @@sprained_ankle_game.updated_state(user_state)
@@ -51,7 +58,7 @@ class SprainedAnkleController < ApplicationController
 		end
 	else
 		@watson = watson_response
-		@@sprained_ankle_game.set_game_output(user_input)
+		@@sprained_ankle_game.set_game_output("You <div class='card'>" + user_input + "</div>")
 		user_state = @@sprained_ankle_game.get_updated_state(watson_response)
 
 		if user_state
--- app/helpers/sprained_ankle_helper.rb
+++ app/helpers/sprained_ankle_helper.rb
@@ -64,73 +64,79 @@ module SprainedAnkleHelper
 
 		  def self.initialize_actions_to_response_narrator()
 		  	# This is a mapping from an action to a response message.
-			@@actions_to_responses_narrator['Start'] = "<p>You've heard that a certain high powered businessman " +
-			"is having an extended stay at Northwest General.</p> <p>He's the one that's responsible " +
-			"for kicking your dog. It's time to give him a piece of your mind. </p><p> Of course, you're " +
-			"gonna get nowhere just going into the hospital and demanding to see him. No, you'll have " +
-			"to be more clever than that.</p> <p>You enter the first floor of the hospital. You've " +
-			"picked up a white coat from a costume shop, swiped a stethoscope off some resident's desk, " +
-			"and are equipped to the brim with the best bandaids $5 at CVS could buy.</p><p> Oh yeah, and " +
-			"WATSON, you have him too.</p><p>You're ready.</p><h2>First Floor - Northwest Hospital</h2>"
+			@@actions_to_responses_narrator['Start'] = "Narrator <div class='card' style='margin-bottom: " +
+			"20px;'><p>You've heard that a certain high powered businessman is having an extended stay " +
+			"at Northwest General.</p> <p>He's the one that's responsible for kicking your dog. It's time " +
+			" to give him a piece of your mind. </p><p> Of course, you're gonna get nowhere just going " +
+			"into the hospital and demanding to see him. No, you'll have to be more clever than that." +
+			"</p> <p>You enter the first floor of the hospital. You've picked up a white coat from a " +
+			"costume shop, swiped a stethoscope off some resident's desk, and are equipped to the brim " +
+			"with the best bandaids $5 at CVS could buy.</p><p> Oh yeah, and WATSON, you have him too." +
+			"</p><p>You're ready.</p></div>Location <div class='card' style='margin-top: 8px;'>" +
+			"<p>First Floor - Northwest Hospital</p></div>"
 
-			@@actions_to_responses_narrator['Remedy'] = "Correct! The patient does have a sprained " + 
-			"ankle."
+			@@actions_to_responses_narrator['Remedy'] = "Narrator <div class='card'>Correct! The patient " +
+			"does have a sprained ankle.</div>"
 
-			@@actions_to_responses_narrator['Done'] = "You've completed level 1. Congratulations!"
+			@@actions_to_responses_narrator['Done'] = "Narrator <div class='card'>You've completed level " +
+			"1. Congratulations!</div>"
 		  end 
 
 		  def self.initialize_actions_to_response_nurse()
 		  	# This is a mapping from an action to a response message.
-			@@actions_to_responses_nurse['Start'] = "<p>Hey you! You're new here, but the ER is overflowing, " +
-			"could you take a look at this guy? I'm sure it'll only take a few seconds...</p>"
-			@@actions_to_responses_nurse['Name'] = "<p>Awesome, thanks! What's your name by the way?</p>"
-			@@actions_to_responses_nurse['Get Situation'] = "Thanks, #{@username}. <p class='xdelay'> " +
-			"Hey, #{@username}! Get over here!</p>"
-			@@actions_to_responses_nurse['Injury Details'] = "Ouch, that looks pretty bad."
-			@@actions_to_responses_nurse['Remedy'] = "Great diagnosis, doc! Now let's offer the patient " +
-			"some remedies for his sprained ankle."
+			@@actions_to_responses_nurse['Start'] = "Nurse <div class='card'><p>Hey you! You're new here, " +
+			"but the ER is overflowing, could you take a look at this guy? I'm sure it'll only take a " +
+			"few seconds...</p></div>"
+			@@actions_to_responses_nurse['Name'] = "Nurse <div class='card'><p>Awesome, thanks! What's " +
+			"your name by the way?</p></div>"
+			@@actions_to_responses_nurse['Get Situation'] = "Nurse <div class='card'>Thanks, #{$username}. " +
+			"<p class='xdelay'> Hey, #{$username}! Get over here!</p></div>"
+			@@actions_to_responses_nurse['Injury Details'] = "Nurse <div class='card'>Ouch, that looks " +
+			"pretty bad.</div>"
+			@@actions_to_responses_nurse['Remedy'] = "Nurse <div class='card'>Great diagnosis, doc! Now let's " +
+			"offer the patient some remedies for his sprained ankle.</div>"
 
-			@@actions_to_responses_nurse['Done'] = "#{@username}, there's another patient on the next " +
-			"floor that needs to see someone immediately, but all of the other doctors are busy, mind if " +
-			"you give me a hand?"
+			@@actions_to_responses_nurse['Done'] = "Nurse <div class='card'>#{$username}, there's " +
+			"another patient on the next floor that needs to see someone immediately, but all of the " +
+			"other doctors are busy, mind if you give me a hand?</div>"
 		  end 
 
 		  def self.initialize_actions_to_response_watson()
 		  	# This is a mapping from an action to a response message.
-			@@actions_to_responses_watson['Get Situation'] = "Wow, you've really gotten yourself in quite the " +
-			"predicament there #{@username}. Well, I'll tell you what, getting revenge on the guy that " +
-			"kicked your dog is a pretty admirable goal you got there. I'll make myself available to you. " +
-			"Whenever you have a question, just ask me it and I'll try and grab some info for you. " +
-			"You'll be able to navigate the hospital by telling me where you want to go, but of course " +
-			"some locations aren't gonna be available until you get past certain people. Use your wit " +
-			"and my knowledge to get past these obstacles and get yourself up to the 5th floor. I've " +
-			"heard that's where he's staying. <p class='delay'>Psst, ask her what's wrong</p>"
-			@@actions_to_responses_watson['Injury Details'] = "I'm not too sure that's a broken ankle. Why don't " +
-			"you try asking him how he got this injury?"
-			@@actions_to_responses_watson['Move Ankle'] = "Alright, that certainly doesn't sound like a broken " +
-			"ankle."
-			@@actions_to_responses_watson['Diagnosis'] = "<p>It's time to give a diagnosis. Make sure you " +
-			"do this properly! </p> "
+			@@actions_to_responses_watson['Get Situation'] = "Watson <div class='card'>Wow, you've " +
+			"really gotten yourself in quite the predicament there #{$username}. Well, I'll tell you " +
+			"what, getting revenge on the guy that kicked your dog is a pretty admirable goal you got " +
+			"there. I'll make myself available to you. Whenever you have a question, just ask me it and " +
+			"I'll try and grab some info for you. You'll be able to navigate the hospital by telling me " +
+			"where you want to go, but of course some locations aren't gonna be available until you get " +
+			"past certain people. Use your wit and my knowledge to get past these obstacles and get " +
+			"yourself up to the 5th floor. I've heard that's where he's staying. <p class='delay'>Psst, " +
+			"ask her what's wrong</p></div>"
+			@@actions_to_responses_watson['Injury Details'] = "Watson <div class='card'>I'm not too sure " +
+			"that's a broken ankle. Why don't you try asking him how he got this injury?</div>"
+			@@actions_to_responses_watson['Move Ankle'] = "Watson <div class='card'>Alright, that " +
+			"certainly doesn't sound like a broken ankle.</div>"
+			@@actions_to_responses_watson['Diagnosis'] = "Watson <div class='card'><p>It's time to " +
+			"give a diagnosis. Make sure you do this properly! </p></div>"
 
-			@@actions_to_responses_watson['Remedy'] = "<p>If you have any questions about how to remedy " +
-			"a sprained ankle, ask me!</p>"
+			@@actions_to_responses_watson['Remedy'] = "Watson <div class='card'><p>If you have any " +
+			"questions about how to remedy a sprained ankle, ask me!</p></div>"
 
-			@@actions_to_responses_watson['Done'] = "Great job, partner! We make a good team!  I think " +
-			"you've even convinced that nurse."
+			@@actions_to_responses_watson['Done'] = "Watson <div class='card'>Great job, partner! We " +
+			"make a good team!  I think you've even convinced that nurse.</div>"
 		  end 
 
 		  def self.initialize_actions_to_response_patient()
 		  	# This is a mapping from an action to a response message.
-			@@actions_to_responses_patient['Start'] = ""
-			@@actions_to_responses_patient['Name'] = ""
-			@@actions_to_responses_patient[''] = ""
-			@@actions_to_responses_patient['Get Situation'] = "OWWWWWW!"
-			@@actions_to_responses_patient['Examine'] = "I don't know... That's why I came here. I think " +
-			"I broke my ankle, it really hurts when I put pressure on it. "
-			@@actions_to_responses_patient['Move Ankle'] = " I was playing basketball... I was going up for a " +
-			"layup when I got hit and had to land really awkwardly."
-			@@actions_to_responses_patient['Diagnosis'] = "What do you think I have, doc?"
-			@@actions_to_responses_patient['Done'] = "Thank you, Doc!"
+			@@actions_to_responses_patient['Get Situation'] = "Patient <div class='card'>OWWWWWW!</div>"
+			@@actions_to_responses_patient['Examine'] = "Patient <div class='card'>I don't know... " +
+			"That's why I came here. I think I broke my ankle, it really hurts when I put pressure on " +
+			"it.</div>"
+			@@actions_to_responses_patient['Move Ankle'] = "Patient <div class='card'>I was playing " +
+			"basketball... I was going up for a layup when I got hit and had to land really awkwardly.</div>"
+			@@actions_to_responses_patient['Diagnosis'] = "Patient <div class='card'>What do you think " +
+			"I have, doc?</div>"
+			@@actions_to_responses_patient['Done'] = "Patient <div class='card'>Thank you, Doc!</div>"
 		  end 
 
 		  def self.initialize_states_to_actions()
@@ -167,18 +173,22 @@ module SprainedAnkleHelper
 		  end
 
 		  def self.initialize_bad_response_to_state()
-		  	@@bad_response_to_state['Start'] = "Oh c'mon! Let's play doctor."
-		  	@@bad_response_to_state['Get Situation'] = "If you're going to be a doctor, you should probably " +
-		  	"start by seeing what is wrong with your patient!"
-		  	@@bad_response_to_state['Examine'] = "Maybe you should take a further look at this situation?"
-		  	@@bad_response_to_state['Injury Details'] = "That's not what you should be doing right " +
-		  	"now... You'll get caught if you don't act like a real doctor"
-		  	@@bad_response_to_state['Move Ankle'] = "Wrong move! Act like a real doctor! Ask the patient to " +
-		  	"move his ankle."
-		  	@@bad_response_to_state['Diagnosis'] = "Ask Watson about common ankle injuries to " +
-		  	"diagnosis this patient."
+		  	@@bad_response_to_state['Start'] = "Bad Response: <div class='card'>Oh c'mon! Let's play " + 
+		  	"doctor.</div>"
+		  	@@bad_response_to_state['Get Situation'] = "<div class='card'>If you're going to be a " +
+		  	"doctor, you should probably start by seeing what is wrong with your patient!</div>"
+		  	@@bad_response_to_state['Examine'] = "Bad Response: <div class='card'>Maybe you should " +
+		  	"take a further look at this situation?</div>"
+		  	@@bad_response_to_state['Injury Details'] = "Bad Response: <div class='card'>That's not " +
+		  	"what you should be doing right now... You'll get caught if you don't act like a real " +
+		  	"doctor</div>"
+		  	@@bad_response_to_state['Move Ankle'] = "Bad Response: <div class='card'>Wrong move! Act " +
+		  	"like a real doctor! Ask the patient to move his ankle.</div>"
+		  	@@bad_response_to_state['Diagnosis'] = "Bad Response: <div class='card'>Ask Watson about " +
+		  	"common ankle injuries to diagnosis this patient.</div>"
 
-		  	@@bad_response_to_state['Remedy'] = "Ask Waston about remedies for sprained ankles!"
+		  	@@bad_response_to_state['Remedy'] = "Bad Response: <div class='card'>Ask Waston about " +
+		  	"remedies for sprained ankles!</div>"
 		  end
 
 		  def get_narrator_response(action)
@@ -200,24 +210,27 @@ module SprainedAnkleHelper
 		  def set_remedy_value(action)
 		  	if action == "Rest"
 		  		if @remedy_values[0] == 1 
-		  			@game_output << "You've already suggested to rest. Any other suggestions?"
+		  			@game_output << "Bad Response <div class='card'><p>You've already suggested to rest. " +
+		  			" Any other suggestions?</p></div>"
 		  		else
 		  			@remedy_values[0] = 1
-		  			@game_output << "Okay, what's next?"
+		  			@game_output << "Patient <div class='card'><p>Okay, what's next?</p></div>"
 		  		end
 		  	elsif action == "Elevate Ankle"
 		  		if @remedy_values[1] == 1
-		  			@game_output << "You've already suggested to elevate ankle. Any other suggestions?"
+		  			@game_output << "Bad Response <div class='card'><p>You've already suggested to elevate " +
+		  			"ankle. Any other suggestions?</p></div>"
 		  		else
 		  			@remedy_values[1] = 1
-		  			@game_output << "What else can I do?"	
+		  			@game_output << "Patient <div class='card'><p>What else can I do?</p></div>"	
 		  		end
 		  	else action == "Ice"
 		  		if @remedy_values[2] == 1
-		  			@game_output << "You've already suggested to elevate ankle. Any other suggestions?"
+		  			@game_output << "Bad Response <div class='card'><p>You've already suggested to elevate " +
+		  			"ankle. Any other suggestions?</p></div>"
 		  		else 
 		  			@remedy_values[2] = 1
-		  			@game_output << "Okay, anything else?"
+		  			@game_output << "Patient <div class='card'><p>Okay, anything else?</p></div>"
 		  		end
 		  	end
 		  end
